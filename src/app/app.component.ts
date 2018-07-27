22297e353910b75c03af3ccb65294f683e1d2948
Danilo Bizarria <danilobizarria@gmail.com>
2018-07-26 21:27:14 -0300

    arquivos antes não monitorados

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'projeto1';
+  title = 'tutorial';
+
+  chamarHeroes(): void {
+    // chamar a  página de heroes
+  }
 }
